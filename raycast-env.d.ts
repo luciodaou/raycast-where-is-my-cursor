9d651200340b506fdba2bde7a2275cc4087dc15c
Lucio Daou <53634214+luciodaou@users.noreply.github.com>
2025-08-13 16:12:14 -0300

    Updated

--- raycast-env.d.ts
+++ raycast-env.d.ts
@@ -15,10 +15,18 @@ declare type Preferences = ExtensionPreferences
 declare namespace Preferences {
   /** Preferences accessible in the `where-is-my-cursor` command */
   export type WhereIsMyCursor = ExtensionPreferences & {}
+  /** Preferences accessible in the `toggle-dimming` command */
+  export type ToggleDimming = ExtensionPreferences & {}
+  /** Preferences accessible in the `dim-with-duration` command */
+  export type DimWithDuration = ExtensionPreferences & {}
 }
 
 declare namespace Arguments {
   /** Arguments passed to the `where-is-my-cursor` command */
   export type WhereIsMyCursor = {}
+  /** Arguments passed to the `toggle-dimming` command */
+  export type ToggleDimming = {}
+  /** Arguments passed to the `dim-with-duration` command */
+  export type DimWithDuration = {}
 }
 
